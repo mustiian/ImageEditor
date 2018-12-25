06bb92b92fbcc3b6e61a74b1f7958f1a1616648e
mustiian <ianmustiats@seznam.cz>
2018-12-25 13:51:21 +0100

    Add buttons in program

--- imgEditor.py
+++ imgEditor.py
@@ -27,12 +27,60 @@ def createMenuBar(master):
 
 		master.config(menu=menubar)
 
-class App:  
-	def __init__(self, master):
+def createButtons(master):
 		master_frame = Frame(master)
 		master_frame.pack()
+
+		btn_frame = Frame(master_frame, relief=SUNKEN, borderwidth=2)
+		btn_frame.pack(fill=BOTH, expand=1)
 		
+		button_invert = Button(
+			btn_frame, font="Arial 10", text="Invert color", command=btn_frame.quit
+		)
+		button_invert.pack(padx=5, pady=5)
+		
+		button_grayscale = Button(
+			btn_frame, font="Arial 10", text="Grayscale", command=btn_frame.quit
+		)
+		
+		button_grayscale.pack(padx=5, pady=5)
+
+		button_brightness = Button(
+			btn_frame, font="Arial 10", text="Brightness", command=btn_frame.quit
+		)
+		
+		button_brightness.pack(padx=5, pady=5)
+
+		button_edges = Button(
+			btn_frame, font="Arial 10", text="Edges", command=btn_frame.quit
+		)
+		
+		button_edges.pack(padx=5, pady=5)
+
+		button_show = Button(
+			btn_frame, font="Arial 10", fg='red', text="Show image", command=btn_frame.quit
+		)
+		
+		button_show.pack(padx=5, pady=5)
+
+class App:  
+	def __init__(self, master):
 		createMenuBar(master)
+		createButtons(master)
+
+	def showIMG(self):
+		root = Tk()
+		root.title('Show Image')
+
+		e = Entry(root, font="Arial 16")
+		e.pack(padx=5, pady=5)
+
+		img = Image.open('kvetina.jpg')
+		
+		self.button_show = Button(
+			root, text="Show Image", command=img.show
+		)
+		self.button_show.pack(padx=5, pady=5)
 
 def main():
 	root = Tk()
